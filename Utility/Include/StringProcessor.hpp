215524c5e2be7965e9cc024fc99832a76e5e23a4
SolaireLibrary <solairelibrary@mail.com>
2015-09-11 16:01:18 +0100

    Parse Documentation
    Added documentation for Parse functions
    Added documentation for the internal FSA in ParseNumber

--- Utility/Include/StringProcessor.hpp
+++ Utility/Include/StringProcessor.hpp
@@ -276,117 +276,133 @@ namespace Solaire{ namespace Utility{ namespace Strings{
     }
 
     static const char* ParseNumber(const char* const aBegin, const char* const aEnd, double& aOutput, const bool aCanBeNegative, const bool aCanBeDecimal){
+        // The states for the parsing FSA
         enum State{
-            FIND_SIGN,
-            FIND_VALUE,
-            FIND_DECIMAL_VALUE,
-            FIND_E_SIGN,
-            FIND_E_VALUE,
-            RETURN
+            FIND_SIGN,                          // Initial state, determines if the value is positive or negative
+            FIND_VALUE,                         // Looking for the body of the value
+            FIND_DECIMAL_VALUE,                 // Looking for the decimal places in the value
+            FIND_EXPONENT_SIGN,                 // Determines if an exponent is in the form eN, e+N or e-N
+            FIND_EXPONENT_VALUE,                // Looking for the exponent value
+            RETURN                              // Final parsing of the value for return
         };
 
-        const size_t length = aEnd - aBegin;
-        bool negative = false;
-        bool eNegative = false;
-        int64_t eValue = 0;
-        int64_t value = 0;
-        int64_t decimalValue = 0;
-        uint32_t decimalPlaces = 0;
-        uint8_t digit;
-        const char* eBegin = nullptr;
-        const char* decimalBegin = nullptr;
-        const char* i = aBegin;
-        State state = State::FIND_SIGN;
+        bool negative = false;                  // Set to true if the value is determined to be negative
+        bool exponentNegative = false;          // Set to true if the exponent is determined to be e-N
 
+        int64_t value = 0;                      // The main value body
+        int64_t exponentValue = 0;              // The value of the exponent
+        int64_t decimalValue = 0;               // The value of the decimal places
+
+        const char* valueBegin = nullptr;       // Set to the first character in the value
+        const char* exponentBegin = nullptr;    // Set to the first character in the exponent value
+        const char* decimalBegin = nullptr;     // Set to the first character in the decimal value
+        const char* i = aBegin;                 // The current character
+
+        uint32_t decimalPlaces = 0;             // Stores how many decimal places are in the decimal value
+        uint8_t digit;                          // Captures the output of ParseDigit
+        State state = State::FIND_SIGN;         // The current FSA state
+
+        // Internal FSM logic escapes the loop
         while(true){
+            // If there are no more characters to examine jump straight to the RETURN state
             if(i >= aEnd) state = RETURN;
 
             switch(state){
             case FIND_SIGN:
-                if(*i == '-'){
-                    negative = true;
-                    ++i;
+                if(*i == '-'){          // Check if the current character is a sign
+                    negative = true;    // Set the negative value flag
+                    ++i;                // Move to the next character, which should be the first number in the value
                 }
-                state = FIND_VALUE;
+                valueBegin = i;
+                state = FIND_VALUE;     // Jump to the FIND_VALUE state
                 break;
             case FIND_VALUE:
-                if(*i == 'e' || *i == 'E'){
-                    state = FIND_E_SIGN;
-                    ++i;
-                }else if(*i == '.'){
-                    state = FIND_DECIMAL_VALUE;
-                    decimalBegin = i;
-                    ++i;
+                if(*i == 'e' || *i == 'E'){         // Check for an exponent
+                    ++i;                            // Move to the next character, which should be the first number in the exponent, or it's sign
+                    state = FIND_EXPONENT_SIGN;     // Jump to the FIND_EXPONENT_SIGN state
+                }else if(*i == '.'){                // Check for a decimal place
+                    ++i;                            // Move to the next character, which should be the first number in the decimal value
+                    decimalBegin = i;               // Record which character the decimal value stated from
+                    state = FIND_DECIMAL_VALUE;     // Jump to the FIND_DECIMAL_VALUE state
                 }else{
-                    if(! ParseDigit(*i, digit)){
-                        --i;
-                        state = RETURN;
+                    if(! ParseDigit(*i, digit)){    // Check if the value is not a valid number
+                        --i;                        // Move back the the previous character, which was the last character used in the value
+                        state = RETURN;             // Jump to the return state
                     }else{
-                        value += static_cast<int64_t>(digit) * 10 * ((i - aBegin) + 1);
-                        ++i;
+                        const int64_t offset = (i - valueBegin) + 1;            // Calculate the offset from the beginning of the value
+                        value += static_cast<int64_t>(digit) * 10 * offset;     // Update the value with the new character
+                        ++i;                                                    // Move to the next number in the value
                     }
                 }
                 break;
             case FIND_DECIMAL_VALUE:
-                if(*i == 'e' || *i == 'E'){
-                    state = FIND_E_SIGN;
-                    ++i;
+                if(*i == 'e' || *i == 'E'){         // Check for an exponent
+                    ++i;                            // Move to the next character, which should be the first number in the exponent, or it's sign
+                    state = FIND_EXPONENT_SIGN;     // Jump to the FIND_EXPONENT_SIGN state
                 }else{
-                    if(! ParseDigit(*i, digit)){
-                        --i;
-                        state = RETURN;
+                    if(! ParseDigit(*i, digit)){    // Check if the value is not a valid number
+                        --i;                        // Move back the the previous character, which was the last character used in the decimal value
+                        state = RETURN;             // Jump to the return state
                     }else{
-                        decimalValue += static_cast<int64_t>(digit) * 10 * ((i - decimalBegin) + 1);
-                        ++decimalPlaces;
-                        ++i;
+                        const int64_t offset = (i - decimalBegin) + 1;                  // Calculate the offset from the beginning of the decimal value
+                        decimalValue += static_cast<int64_t>(digit) * 10 * offset;      // Update the decimal value with the new character
+                        ++i;                                                            // Move to the next number in the decimal
                     }
                 }
                 break;
-            case FIND_E_SIGN:
-                if(*i == '-'){
-                    eNegative = true;
-                    ++i;
-                }else if(*i == '+'){
-                    eNegative = false;
-                    ++i;
-                }
-                eBegin = i;
-                state = FIND_E_VALUE;
-                break;
-            case FIND_E_VALUE:
-                if(! ParseDigit(*i, digit)){
-                    --i;
-                    state = RETURN;
+            case FIND_EXPONENT_SIGN:
+                if(*i == '-'){                  // Check for an exponent sign
+                    exponentNegative = true;    // Set the exponent sign flag
+                    ++i;                        // Move to the next character, which should be the first number in the exponent value
+                }else if(*i == '+'){            // Check for an exponent sign
+                    exponentNegative = false;   // Set the exponent sign flag
+                    ++i;                        // Check for an exponent sign
                 }else{
-                    eValue += static_cast<int64_t>(digit) * 10 * ((i - eBegin) + 1);
-                    ++i;
+                    exponentNegative = false;   // Set the exponent sign flag
+                }
+                exponentBegin = i;              // Record which character the exponent value started from
+                state = FIND_EXPONENT_VALUE;    // Jump to the FIND_EXPONENT_VALUE state
+                break;
+            case FIND_EXPONENT_VALUE:
+                if(! ParseDigit(*i, digit)){    // Check if the character is not a valid number
+                    --i;                        // Move back the the previous character, which was the last character used in the exponent value
+                    state = RETURN;             // Jump to the return state
+                }else{
+                    const int64_t offset = (i - exponentBegin) + 1;                  // Calculate the offset from the beginning of the exponent value
+                    exponentValue += static_cast<int64_t>(digit) * 10 * offset;      // Update the exponent value with the new character
+                    ++i;                                                             // Move to the next number in the exponent value
                 }
                 break;
             case RETURN:
                 if(
-                   (i == aBegin) ||
-                   (negative && ! aCanBeNegative) ||
-                   (decimalValue != 0 && ! aCanBeDecimal) ||
-                   (decimalBegin != nullptr && decimalPlaces == 0)
+                   (valueBegin == nullptr) ||                           // Check if the parse has failed
+                   (decimalBegin != nullptr && decimalPlaces == 0) ||
+                   (negative && ! aCanBeNegative) ||                    // Check if the parse has produced an illegal number
+                   (decimalValue != 0 && ! aCanBeDecimal)
                 ){
-                    aOutput = 0;
+                    // Return parse failue
+                    aOutput = 0.0;
                     return nullptr;
                 }else{
+                    // Construct the value body
                     double val = value;
-
                     if(negative) val *= -1.0;
 
-                    if(decimalPlaces > 0){
+                    if(decimalBegin != nullptr){        // Check if a decimal value was parsed
+                        // Add the decimal value
                         double dec = decimalValue;
-                        dec /= 10 * decimalPlaces;
+                        dec /= 10.0 * decimalPlaces;    // Shift the decimal places down
                         val += dec;
                     }
 
-                    if(eValue != 0){
-                        if(eNegative) eValue *= -1;
-                        val *= std::pow(10, eValue);
+                    if(exponentBegin != nullptr){       // Check if an exponent value was parsed
+                        // Multiply by the exponent
+                        int64_t exp = exponentValue;
+                        if(exponentNegative) exp *= -1;
+                        val *= std::pow(10, exp);
                     }
 
+                    // Return parse success
                     aOutput = val;
                     return i;
                 }
@@ -395,6 +411,14 @@ namespace Solaire{ namespace Utility{ namespace Strings{
         }
      }
 
+    /*!
+        \brief Attempt to parse a value from a c-string segment.
+        \tparam T The type to parse.
+        \param aBegin The address of the first character in the segment.
+        \param aBegin The address of the character after the last character in the segment, this will often be be the null terminator (\0).
+        \param aOutput A reference that is assigned the parsed value.
+        \return Nullptr if the parse failed, else the address of the last character used during the pass. This will be equal to \a aEnd-1 if all characters were used.
+    */
     template<typename T>
     static const char* Parse(const char* const aBegin, const char* const aEnd, T& aOutput) = delete;
 
@@ -475,12 +499,20 @@ namespace Solaire{ namespace Utility{ namespace Strings{
         return res;
     }
 
-     template<typename T>
-     static T Parse(const char* const aBegin, const char* const aEnd){
-         T tmp;
-         const char* const res = Parse<T>(aBegin, aEnd, tmp);
-         if(res == nullptr) throw std::runtime_error("Failed to parse value");
-         return tmp;
+    /*!
+        \brief Attempt to parse a value from a c-string segment.
+        \detail Throws std::runtime_error if parse fails.
+        \tparam T The type to parse.
+        \param aBegin The address of the first character in the segment.
+        \param aBegin The address of the character after the last character in the segment, this will often be be the null terminator (\0).
+        \param The parsed value.
+    */
+    template<typename T>
+    static T Parse(const char* const aBegin, const char* const aEnd){
+        T tmp;
+        const char* const res = Parse<T>(aBegin, aEnd, tmp);
+        if(res == nullptr) throw std::runtime_error("Failed to parse value");
+        return tmp;
      }
 }}}
 
