c242adeb80d6956e2d968f2f66d272681289e0fe
SolaireLibrary <solairelibrary@mail.com>
2015-09-30 19:12:21 +0100

    SerialisationInterface

---
+++ Include/Solaire/Core/Serialise/Serialisable.hpp
@@ -0,0 +1,46 @@
+#ifndef SOLAIRE_CORE_SERIALISABLE_HPP
+#define SOLAIRE_CORE_SERIALISABLE_HPP
+
+//Copyright 2015 Adam Smith
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+// Contact :
+// Email             : solairelibrary@mail.com
+// GitHub repository : https://github.com/SolaireLibrary/SolaireCPP
+
+/*!
+	\file Serialisable.hpp
+	\brief
+	\author
+	Created			: Adam Smith
+	Last modified	: Adam Smith
+	\version 1.0
+	\date
+	Created			: 30th September 2015
+	Last Modified	: 30th September 2015
+*/
+
+#include "SerialisationInterface.hpp"
+
+namespace Solaire{ namespace Core{
+
+    template<class T>
+    class Serialisable = delete;
+
+    // T* Read(const SerialSystem& aSystem, std::istream& aStream);
+    // void Write(const T& aValue, SerialSystem& aSystem, std::ostream& aStream);
+}}
+
+
+#endif
---
+++ Include/Solaire/Core/Serialise/SerialisationInterface.hpp
@@ -0,0 +1,162 @@
+#ifndef SOLAIRE_CORE_SERIALISATION_INTERFACE_HPP
+#define SOLAIRE_CORE_SERIALISATION_INTERFACE_HPP
+
+//Copyright 2015 Adam Smith
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+// Contact :
+// Email             : solairelibrary@mail.com
+// GitHub repository : https://github.com/SolaireLibrary/SolaireCPP
+
+/*!
+	\file SerialisationInterface.hpp
+	\brief
+	\author
+	Created			: Adam Smith
+	Last modified	: Adam Smith
+	\version 1.0
+	\date
+	Created			: 30th September 2015
+	Last Modified	: 30th September 2015
+*/
+
+#include <cstdint>
+#include "..\Strings\StringFragment.hpp"
+
+namespace Solaire{ namespace Core{
+
+    typedef Core::ConstStringFragment SerialTag;
+    typedef Core::ConstStringFragment SerialString;
+    typedef size_t SerialIndex;
+
+    class SerialArray;
+    class SerialObject;
+
+    typedef Core::SmartPointer<SerialArray> SerialArrayPtr;
+    typedef Core::SmartPointer<SerialObject> SerialObjectPtr;
+    typedef Core::SmartPointer<const SerialArray> ConstSerialArrayPtr;
+    typedef Core::SmartPointer<const SerialObject> ConstSerialObjectPtr;
+
+    enum SerialType{
+        SERIAL_TYPE_U8,
+        SERIAL_TYPE_U16,
+        SERIAL_TYPE_U32,
+        SERIAL_TYPE_U64,
+        SERIAL_TYPE_I8,
+        SERIAL_TYPE_I16,
+        SERIAL_TYPE_I32,
+        SERIAL_TYPE_I64,
+        SERIAL_TYPE_F,
+        SERIAL_TYPE_D,
+        SERIAL_TYPE_S,
+        SERIAL_TYPE_A,
+        SERIAL_TYPE_O,
+    };
+
+    class SerialSystem{
+    public:
+        virtual ~SerialSystem(){}
+
+        virtual SerialArrayPtr CreateA() const = 0;
+        virtual SerialObjectPtr CreateO() const = 0;
+
+        virtual void WriteA(std::ostream& aStream, const ConstSerialArrayPtr& aArray) const = 0;
+        virtual void WriteO(std::ostream& aStream, const ConstSerialObjectPtr& aArray) const = 0;
+
+        virtual SerialArrayPtr ReadA(std::istream& aStream) const = 0;
+        virtual SerialObjectPtr ReadO(std::istream& aStream) const = 0;
+    };
+
+    class SerialArray{
+    public:
+        virtual ~SerialArray(){}
+
+        virtual SerialIndex Size() const = 0;
+        virtual SerialType TypeOf(const SerialIndex aIndex) const = 0;
+
+        virtual uint8_t ReadU8(const SerialIndex aIndex) const = 0;
+        virtual uint16_t ReadU16(const SerialIndex aIndex) const = 0;
+        virtual uint32_t ReadU32(const SerialIndex aIndex) const = 0;
+        virtual uint64_t ReadU64(const SerialIndex aIndex) const = 0;
+        virtual int8_t ReadI8(const SerialIndex aIndex) const = 0;
+        virtual int16_t ReadI16(const SerialIndex aIndex) const = 0;
+        virtual int32_t ReadI32(const SerialIndex aIndex) const = 0;
+        virtual int64_t ReadI64(const SerialIndex aIndex) const = 0;
+        virtual float ReadF(const SerialIndex aIndex) const = 0;
+        virtual double ReadD(const SerialIndex aIndex) const = 0;
+        virtual SerialString ReadS(const SerialIndex aIndex) const = 0;
+
+        virtual SerialArrayPtr ReadA(const SerialIndex aIndex) = 0;
+        virtual SerialObjectPtr ReadO(const SerialIndex aIndex) = 0;
+        virtual ConstSerialArrayPtr ReadA(const SerialIndex aIndex) const = 0;
+        virtual ConstSerialObjectPtr ReadO(const SerialIndex aIndex) const = 0;
+
+        virtual void WriteU8(const SerialIndex aIndex, const uint8_t aValue) = 0;
+        virtual void WriteU16(const SerialIndex aIndex, const uint16_t aValue) = 0;
+        virtual void WriteU32(const SerialIndex aIndex, const uint32_t aValue) = 0;
+        virtual void WriteU64(const SerialIndex aIndex, const uint64_t aValue) = 0;
+        virtual void WriteI8(const SerialIndex aIndex, const int8_t aValue) = 0;
+        virtual void WriteI16(const SerialIndex aIndex, const int16_t aValue) = 0;
+        virtual void WriteI32(const SerialIndex aIndex, const int32_t aValue) = 0;
+        virtual void WriteI64(const SerialIndex aIndex, const int64_t aValue) = 0;
+        virtual void WriteF(const SerialIndex aIndex, const float aValue) = 0;
+        virtual void WriteD(const SerialIndex aIndex, const double aValue) = 0;
+        virtual void WriteS(const SerialIndex aIndex, const SerialString aValue) = 0;
+
+        virtual void WriteA(const SerialIndex aIndex, SerialArrayPtr) = 0;
+        virtual void WriteO(const SerialIndex aIndex, SerialObjectPtr) = 0;
+    };
+
+    class SerialObject{
+    public:
+        virtual ~SerialObject(){}
+
+        virtual SerialType TypeOf(const SerialTag aTag) const = 0;
+
+        virtual uint8_t ReadU8(const SerialTag aTag) const = 0;
+        virtual uint16_t ReadU16(const SerialTag aTag) const = 0;
+        virtual uint32_t ReadU32(const SerialTag aTag) const = 0;
+        virtual uint64_t ReadU64(const SerialTag aTag) const = 0;
+        virtual int8_t ReadI8(const SerialTag aTag) const = 0;
+        virtual int16_t ReadI16(const SerialTag aTag) const = 0;
+        virtual int32_t ReadI32(const SerialTag aTag) const = 0;
+        virtual int64_t ReadI64(const SerialTag aTag) const = 0;
+        virtual float ReadF(const SerialTag aTag) const = 0;
+        virtual double ReadD(const SerialTag aTag) const = 0;
+        virtual SerialString ReadS(const SerialTag aTag) const = 0;
+
+        virtual SerialArrayPtr ReadA(const SerialTag aTag) = 0;
+        virtual SerialObjectPtr ReadO(const SerialTag aTag) = 0;
+        virtual ConstSerialArrayPtr ReadA(const SerialTag aTag) const = 0;
+        virtual ConstSerialObjectPtr ReadO(const SerialTag aTag) const = 0;
+
+        virtual void WriteU8(const SerialTag aTag, const uint8_t aValue) = 0;
+        virtual void WriteU16(const SerialTag aTag, const uint16_t aValue) = 0;
+        virtual void WriteU32(const SerialTag aTag, const uint32_t aValue) = 0;
+        virtual void WriteU64(const SerialTag aTag, const uint64_t aValue) = 0;
+        virtual void WriteI8(const SerialTag aTag, const int8_t aValue) = 0;
+        virtual void WriteI16(const SerialTag aTag, const int16_t aValue) = 0;
+        virtual void WriteI32(const SerialTag aTag, const int32_t aValue) = 0;
+        virtual void WriteI64(const SerialTag aTag, const int64_t aValue) = 0;
+        virtual void WriteF(const SerialTag aTag, const float aValue) = 0;
+        virtual void WriteD(const SerialTag aTag, const double aValue) = 0;
+        virtual void WriteS(const SerialTag aTag, const SerialString aValue) = 0;
+
+        virtual void WriteA(const SerialTag aTag, SerialArrayPtr) = 0;
+        virtual void WriteO(const SerialTag aTag, SerialObjectPtr) = 0;
+    };
+}}
+
+
+#endif
