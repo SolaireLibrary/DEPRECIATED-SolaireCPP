71fc5ab9cdd6510c1f724c21a53951c8f702ced5
SolaireLibrary <solairelibrary@mail.com>
2015-10-01 14:34:39 +0100

    Templated Read / Write functions for SerialArray / SerialObject

--- Include/Solaire/Core/Serialise/SerialisationInterface.hpp
+++ Include/Solaire/Core/Serialise/SerialisationInterface.hpp
@@ -127,7 +127,41 @@ namespace Solaire{ namespace Core{
 
         virtual void WriteA(const SerialIndex aIndex, SerialArrayPtr) = 0;
         virtual void WriteO(const SerialIndex aIndex, SerialObjectPtr) = 0;
+
+        template<class T>
+        T Read(const SerialIndex aIndex) const = delete;
+
+        template<class T>
+        void Write(const SerialIndex aIndex, const T aValue) = delete;
     };
+
+    template<> bool SerialArray::Read<bool>(const SerialIndex aIndex) const{return ReadB(aIndex);}
+    template<> char SerialArray::Read<char>(const SerialIndex aIndex) const{return ReadC(aIndex);}
+    template<> uint8_t SerialArray::Read<uint8_t>(const SerialIndex aIndex) const{return ReadU8(aIndex);}
+    template<> uint16_t SerialArray::Read<uint16_t>(const SerialIndex aIndex) const{return ReadU16(aIndex);}
+    template<> uint32_t SerialArray::Read<uint32_t>(const SerialIndex aIndex) const{return ReadU32(aIndex);}
+    template<> uint64_t SerialArray::Read<uint64_t>(const SerialIndex aIndex) const{return ReadU64(aIndex);}
+    template<> int8_t SerialArray::Read<int8_t>(const SerialIndex aIndex) const{return ReadI8(aIndex);}
+    template<> int16_t SerialArray::Read<int16_t>(const SerialIndex aIndex) const{return ReadI16(aIndex);}
+    template<> int32_t SerialArray::Read<int32_t>(const SerialIndex aIndex) const{return ReadI32(aIndex);}
+    template<> int64_t SerialArray::Read<int64_t>(const SerialIndex aIndex) const{return ReadI64(aIndex);}
+    template<> float SerialArray::Read<float>(const SerialIndex aIndex) const{return ReadF(aIndex);}
+    template<> double SerialArray::Read<double>(const SerialIndex aIndex) const{return ReadD(aIndex);}
+    template<> SerialString SerialArray::Read<SerialString>(const SerialIndex aIndex) const{return ReadS(aIndex);}
+
+    template<> void SerialArray::Write<bool>(const SerialIndex aIndex, const bool aValue){WriteB(aIndex, aValue);}
+    template<> void SerialArray::Write<char>(const SerialIndex aIndex, const char aValue){WriteC(aIndex, aValue);}
+    template<> void SerialArray::Write<uint8_t>(const SerialIndex aIndex, const uint8_t aValue){WriteU8(aIndex, aValue);}
+    template<> void SerialArray::Write<uint16_t>(const SerialIndex aIndex, const uint16_t aValue){WriteU16(aIndex, aValue);}
+    template<> void SerialArray::Write<uint32_t>(const SerialIndex aIndex, const uint32_t aValue){WriteU32(aIndex, aValue);}
+    template<> void SerialArray::Write<uint64_t>(const SerialIndex aIndex, const uint64_t aValue){WriteU64(aIndex, aValue);}
+    template<> void SerialArray::Write<int8_t>(const SerialIndex aIndex, const int8_t aValue){WriteI8(aIndex, aValue);}
+    template<> void SerialArray::Write<int16_t>(const SerialIndex aIndex, const int16_t aValue){WriteI16(aIndex, aValue);}
+    template<> void SerialArray::Write<int32_t>(const SerialIndex aIndex, const int32_t aValue){WriteI32(aIndex, aValue);}
+    template<> void SerialArray::Write<int64_t>(const SerialIndex aIndex, const int64_t aValue){WriteI64(aIndex, aValue);}
+    template<> void SerialArray::Write<float>(const SerialIndex aIndex, const float aValue){WriteF(aIndex, aValue);}
+    template<> void SerialArray::Write<double>(const SerialIndex aIndex, const double aValue){WriteD(aIndex, aValue);}
+    template<> void SerialArray::Write<SerialString>(const SerialIndex aIndex, const SerialString aValue){WriteS(aIndex, aValue);}
 
     class SerialObject : public std::enable_shared_from_this<SerialObject>{
     public:
@@ -171,7 +205,41 @@ namespace Solaire{ namespace Core{
 
         virtual void WriteA(const SerialTag aTag, SerialArrayPtr) = 0;
         virtual void WriteO(const SerialTag aTag, SerialObjectPtr) = 0;
+
+        template<class T>
+        T Read(const SerialTag aTag) const = delete;
+
+        template<class T>
+        void Write(const SerialTag aTag, T aValue) = delete;
     };
+
+    template<> bool SerialObject::Read<bool>(const SerialTag aTag) const{return ReadB(aTag);}
+    template<> char SerialObject::Read<char>(const SerialTag aTag) const{return ReadC(aTag);}
+    template<> uint8_t SerialObject::Read<uint8_t>(const SerialTag aTag) const{return ReadU8(aTag);}
+    template<> uint16_t SerialObject::Read<uint16_t>(const SerialTag aTag) const{return ReadU16(aTag);}
+    template<> uint32_t SerialObject::Read<uint32_t>(const SerialTag aTag) const{return ReadU32(aTag);}
+    template<> uint64_t SerialObject::Read<uint64_t>(const SerialTag aTag) const{return ReadU64(aTag);}
+    template<> int8_t SerialObject::Read<int8_t>(const SerialTag aTag) const{return ReadI8(aTag);}
+    template<> int16_t SerialObject::Read<int16_t>(const SerialTag aTag) const{return ReadI16(aTag);}
+    template<> int32_t SerialObject::Read<int32_t>(const SerialTag aTag) const{return ReadI32(aTag);}
+    template<> int64_t SerialObject::Read<int64_t>(const SerialTag aTag) const{return ReadI64(aTag);}
+    template<> float SerialObject::Read<float>(const SerialTag aTag) const{return ReadF(aTag);}
+    template<> double SerialObject::Read<double>(const SerialTag aTag) const{return ReadD(aTag);}
+    template<> SerialString SerialObject::Read<SerialString>(const SerialTag aTag) const{return ReadS(aTag);}
+
+    template<> void SerialObject::Write<bool>(const SerialTag aTag, const bool aValue){WriteB(aTag, aValue);}
+    template<> void SerialObject::Write<char>(const SerialTag aTag, const char aValue){WriteC(aTag, aValue);}
+    template<> void SerialObject::Write<uint8_t>(const SerialTag aTag, const uint8_t aValue){WriteU8(aTag, aValue);}
+    template<> void SerialObject::Write<uint16_t>(const SerialTag aTag, const uint16_t aValue){WriteU16(aTag, aValue);}
+    template<> void SerialObject::Write<uint32_t>(const SerialTag aTag, const uint32_t aValue){WriteU32(aTag, aValue);}
+    template<> void SerialObject::Write<uint64_t>(const SerialTag aTag, const uint64_t aValue){WriteU64(aTag, aValue);}
+    template<> void SerialObject::Write<int8_t>(const SerialTag aTag, const int8_t aValue){WriteI8(aTag, aValue);}
+    template<> void SerialObject::Write<int16_t>(const SerialTag aTag, const int16_t aValue){WriteI16(aTag, aValue);}
+    template<> void SerialObject::Write<int32_t>(const SerialTag aTag, const int32_t aValue){WriteI32(aTag, aValue);}
+    template<> void SerialObject::Write<int64_t>(const SerialTag aTag, const int64_t aValue){WriteI64(aTag, aValue);}
+    template<> void SerialObject::Write<float>(const SerialTag aTag, const float aValue){WriteF(aTag, aValue);}
+    template<> void SerialObject::Write<double>(const SerialTag aTag, const double aValue){WriteD(aTag, aValue);}
+    template<> void SerialObject::Write<SerialString>(const SerialTag aTag, const SerialString aValue){WriteS(aTag, aValue);}
 }}
 
 
