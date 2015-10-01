8d3b2c29e4cf65326417e5e23df45e31c2eb409a
SolaireLibrary <solairelibrary@mail.com>
2015-10-01 19:21:31 +0100

    Math optimise

--- Include/Solaire/Core/Maths.hpp
+++ Include/Solaire/Core/Maths.hpp
@@ -37,28 +37,56 @@ namespace Solaire{ namespace Core{ namespace Maths{
 
     namespace MathsInternal{
         static constexpr char BIT_PATTERNS[16][5]{
-            {"0000"}, {"0001"}, {"0010"}, {"0011"},
-            {"0100"}, {"0101"}, {"0110"}, {"0111"},
-            {"1000"}, {"1001"}, {"1010"}, {"1011"},
-            {"1100"}, {"1101"}, {"1110"}, {"1111"},
+            {"0000"}, {"0001"}, {"0010"}, {"0011"}, {"0100"}, {"0101"}, {"0110"}, {"0111"},
+            {"1000"}, {"1001"}, {"1010"}, {"1011"}, {"1100"}, {"1101"}, {"1110"}, {"1111"},
         };
-
-        static constexpr uint8_t PopCount(const char* const aPattern){
-            return (aPattern[0] == '1' ? 1 : 0) + (aPattern[1] == '1' ? 1 : 0) + (aPattern[2] == '1' ? 1 : 0) + (aPattern[3] == '1' ? 1 : 0);
-        }
 
         static constexpr uint8_t BIT_COUNTS[16]{
-            PopCount(BIT_PATTERNS[0]), PopCount(BIT_PATTERNS[1]), PopCount(BIT_PATTERNS[2]), PopCount(BIT_PATTERNS[3]),
-            PopCount(BIT_PATTERNS[4]), PopCount(BIT_PATTERNS[5]), PopCount(BIT_PATTERNS[6]), PopCount(BIT_PATTERNS[7]),
-            PopCount(BIT_PATTERNS[8]), PopCount(BIT_PATTERNS[9]), PopCount(BIT_PATTERNS[10]), PopCount(BIT_PATTERNS[11]),
-            PopCount(BIT_PATTERNS[12]), PopCount(BIT_PATTERNS[13]), PopCount(BIT_PATTERNS[14]), PopCount(BIT_PATTERNS[15])
+            0,	1,	1,	2,	1,	2,	2,	3,
+            1,	2,	2,	3,	2,	3,	3,	4
         };
+
+        static constexpr uint8_t REFLECTED_BYTES[256] = {
+            0,	    128,	64,	    192,	32,	    160,	96,	    224,	16,	    144,
+            80,	    208,	48,	    176,	112,	240,	8,	    136,	72,	    200,
+            40,	    168,	104,	232,	24,	    152,	88,	    216,	56,     184,
+            120,	248,	4,	    132,	68,	    196,	36,	    164,	100,	228,
+            20,	    148,	84,	    212,	52,	    180,	116,	244,	12,	    140,
+            76,	    204,	44,	    172,	108,	236,	28,	    156,	92,	    220,
+            60,	    188,	124,	252,	2,	    130,	66,	    194,	34,	    162,
+            98,	    226,	18,	    146,	82,	    210,	50,	    178,	114,	242,
+            10,	    138,	74,	    202,	42,	    170,	106,	234,	26,	    154,
+            90, 	218,	58,	    186,	122,	250,	6,	    134,	70,	    198,
+            38,	    166,	102,	230,	22,	    150,	86,	    214,	54,	    182,
+            118,	246,	14,	    142,	78,	    206,	46,	    174,	110,	238,
+            30,	    158,	94,	    222,	62,	    190,	126,	254,	1,	    129,
+            65,	    193,	33,	    161,	97,	    225,	17,	    145,	81,	    209,
+            49,	    177,	113,	241,	9,	    137,	73,	    201,	41,	    169,
+            105,	233,	25,	    153,	89,	    217,	57,	    185,	121,	249,
+            5,	    133,	69,	    197,	37,	    165,	101,	229,	21,	    149,
+            85,	    213,	53,	    181,	117,	245,	13,	    141,	77,	    205,
+            45,	    173,	109,	237,	29,	    157,	93,	    221,	61,	    189,
+            125,	253,	3,	    131,	67,	    195,	35,	    163,	99,	    227,
+            19,	    147,	83,	    211,	51,	    179,	115,	243,	11,	    139,
+            75,	    203,	43,	    171,	107,	235,	27,	    155,	91,	    219,
+            59,	    187,	123,	251,	7,	    135,	71,	    199,	39,	    167,
+            103,	231,	23,	    151,	87,	    215,	55,	    183,	119,	247,
+            15,	    143,	79,	    207,	47,	    175,	111,	239,	31,	    159,
+            95,	    23,	    63,	    191,	127,	255
+        };
+    }
+
+    // Cast
+
+    template<class A, class B, typename Enable = typename std::enable_if<sizeof(A) == sizeof(B)>::type>
+    static constexpr A IntactCast(const B aValue){
+        return *reinterpret_cast<const B*>(&aValue);
     }
 
     // Word Length
 
     static constexpr uint8_t Lower4(const uint8_t aValue){
-        return aValue & 15;
+        return aValue & 0xF;
     }
 
     static constexpr uint8_t Upper4(const uint8_t aValue){
@@ -66,7 +94,7 @@ namespace Solaire{ namespace Core{ namespace Maths{
     }
 
     static constexpr uint8_t Lower8(const uint16_t aValue){
-        return aValue & UINT8_MAX;
+        return aValue & 0xFF;
     }
 
     static constexpr uint8_t Upper8(const uint16_t aValue){
@@ -74,7 +102,7 @@ namespace Solaire{ namespace Core{ namespace Maths{
     }
 
     static constexpr uint16_t Lower16(const uint32_t aValue){
-        return aValue & UINT16_MAX;
+        return aValue & 0xFFFF;
     }
 
     static constexpr uint16_t Upper16(const uint32_t aValue){
@@ -82,33 +110,102 @@ namespace Solaire{ namespace Core{ namespace Maths{
     }
 
     static constexpr uint32_t Lower32(const uint64_t aValue){
-        return aValue & UINT32_MAX;
+        return aValue & 0xFFFFFFFFL;
     }
 
     static constexpr uint32_t Upper32(const uint64_t aValue){
         return aValue >> 32L;
     }
 
+    // Combine
+
+    static constexpr uint16_t Combine16(const uint8_t a0, const uint32_t a1){
+        return (static_cast<uint16_t>(a0) << 8) | a1;
+    }
+
+    static constexpr uint32_t Combine32(
+        const uint8_t a0, const uint32_t a1, const uint8_t a2, const uint32_t a3
+    ){
+        return
+            (static_cast<uint32_t>(a0) << 24) |
+            (static_cast<uint32_t>(a1) << 16) |
+            (static_cast<uint32_t>(a2) << 8) |
+            static_cast<uint32_t>(a3);
+    }
+
+    static constexpr uint64_t Combine64(
+        const uint8_t a0, const uint32_t a1, const uint8_t a2, const uint32_t a3,
+        const uint8_t a4, const uint32_t a5, const uint8_t a6, const uint32_t a7
+    ){
+        return
+            (static_cast<uint64_t>(a0) << 56L) |
+            (static_cast<uint64_t>(a1) << 48L) |
+            (static_cast<uint64_t>(a2) << 40L) |
+            (static_cast<uint64_t>(a3) << 32L) |
+            (static_cast<uint64_t>(a4) << 24L) |
+            (static_cast<uint64_t>(a5) << 16L) |
+            (static_cast<uint64_t>(a6) << 8L) |
+            static_cast<uint64_t>(a7);
+    }
+
+
     // PopCount
 
-    static constexpr uint32_t PopCountNybble(const uint8_t aValue){
-        return MathsInternal::BIT_COUNTS[aValue];
+    template<class T>
+    static constexpr uint8_t PopCount(const T aValue) = delete;
+
+    template<>
+    constexpr uint8_t PopCount<uint8_t>(const uint8_t aValue){
+        return MathsInternal::BIT_COUNTS[Lower4(aValue)] + MathsInternal::BIT_COUNTS[Upper4(aValue)];
     }
 
-    static constexpr uint32_t PopCount(const uint8_t aValue){
-        return PopCountNybble(Lower4(aValue)) + PopCountNybble(Upper4(aValue));
+    template<>
+    constexpr uint8_t PopCount<uint16_t>(const uint16_t aValue){
+      return
+            PopCount<uint8_t>(aValue >> 8) +
+            PopCount<uint8_t>(aValue & 0xFF);
     }
 
-    static constexpr uint32_t PopCount(const uint16_t aValue){
-        return PopCountNybble(Lower8(aValue)) + PopCountNybble(Upper8(aValue));
+    template<>
+    constexpr uint8_t PopCount<uint32_t>(const uint32_t aValue){
+      return
+            PopCount<uint8_t>(aValue >> 24) +
+            PopCount<uint8_t>(aValue >> 16) +
+            PopCount<uint8_t>(aValue >> 8) +
+            PopCount<uint8_t>(aValue & 0xFF);
     }
 
-    static constexpr uint32_t PopCount(const uint32_t aValue){
-        return PopCountNybble(Lower16(aValue)) + PopCountNybble(Upper16(aValue));
+    template<>
+    constexpr uint8_t PopCount<uint64_t>(const uint64_t aValue){
+        return
+            PopCount<uint8_t>(aValue >> 56L) +
+            PopCount<uint8_t>(aValue >> 48L) +
+            PopCount<uint8_t>(aValue >> 40L) +
+            PopCount<uint8_t>(aValue >> 32L) +
+            PopCount<uint8_t>(aValue >> 24L) +
+            PopCount<uint8_t>(aValue >> 16L) +
+            PopCount<uint8_t>(aValue >> 8L) +
+            PopCount<uint8_t>(aValue & 0xFFL);
     }
 
-    static constexpr uint32_t PopCount(const uint64_t aValue){
-        return PopCountNybble(Lower32(aValue)) + PopCountNybble(Upper32(aValue));
+    template<>
+    constexpr uint8_t PopCount<int8_t>(const int8_t aValue){
+        return PopCount<uint8_t>(IntactCast<uint8_t>(aValue));
+    }
+
+    template<>
+    constexpr uint8_t PopCount<int16_t>(const int16_t aValue){
+        return PopCount<uint16_t>(IntactCast<uint16_t>(aValue));
+    }
+
+    template<>
+    constexpr uint8_t PopCount<int32_t>(const int32_t aValue){
+        return PopCount<uint32_t>(IntactCast<uint32_t>(aValue));
+    }
+
+    template<>
+    constexpr uint8_t PopCount<int64_t>(const int64_t aValue){
+        return PopCount<uint64_t>(IntactCast<uint64_t>(aValue));
     }
 
     // Hex
@@ -206,65 +303,43 @@ namespace Solaire{ namespace Core{ namespace Maths{
         return CeilToClosestMultiple<T>(aValue, aMultiple) - (((aValue & (aMultiple - 1)) < 5) && ((aValue & (aMultiple - 1)) != 0) ? 5 : 0);
     }
 
-    //
-
-    template<class A, class B, typename Enable = typename std::enable_if<sizeof(A) == sizeof(B)>::type>
-    static constexpr A IntactCast(const B aValue){
-        return *reinterpret_cast<const B*>(&aValue);
-    }
-
     // Reflection
-
-    static constexpr uint8_t REFLECTED_BYTES[256] = {
-        0,	    128,	64,	    192,	32,	    160,	96,	    224,	16,	    144,
-        80,	    208,	48,	    176,	112,	240,	8,	    136,	72,	    200,
-        40,	    168,	104,	232,	24,	    152,	88,	    216,	56,     184,
-        120,	248,	4,	    132,	68,	    196,	36,	    164,	100,	228,
-        20,	    148,	84,	    212,	52,	    180,	116,	244,	12,	    140,
-        76,	    204,	44,	    172,	108,	236,	28,	    156,	92,	    220,
-        60,	    188,	124,	252,	2,	    130,	66,	    194,	34,	    162,
-        98,	    226,	18,	    146,	82,	    210,	50,	    178,	114,	242,
-        10,	    138,	74,	    202,	42,	    170,	106,	234,	26,	    154,
-        90, 	218,	58,	    186,	122,	250,	6,	    134,	70,	    198,
-        38,	    166,	102,	230,	22,	    150,	86,	    214,	54,	    182,
-        118,	246,	14,	    142,	78,	    206,	46,	    174,	110,	238,
-        30,	    158,	94,	    222,	62,	    190,	126,	254,	1,	    129,
-        65,	    193,	33,	    161,	97,	    225,	17,	    145,	81,	    209,
-        49,	    177,	113,	241,	9,	    137,	73,	    201,	41,	    169,
-        105,	233,	25,	    153,	89,	    217,	57,	    185,	121,	249,
-        5,	    133,	69,	    197,	37,	    165,	101,	229,	21,	    149,
-        85,	    213,	53,	    181,	117,	245,	13,	    141,	77,	    205,
-        45,	    173,	109,	237,	29,	    157,	93,	    221,	61,	    189,
-        125,	253,	3,	    131,	67,	    195,	35,	    163,	99,	    227,
-        19,	    147,	83,	    211,	51,	    179,	115,	243,	11,	    139,
-        75,	    203,	43,	    171,	107,	235,	27,	    155,	91,	    219,
-        59,	    187,	123,	251,	7,	    135,	71,	    199,	39,	    167,
-        103,	231,	23,	    151,	87,	    215,	55,	    183,	119,	247,
-        15,	    143,	79,	    207,	47,	    175,	111,	239,	31,	    159,
-        95,	    23,	    63,	    191,	127,	255
-    };
 
     template<class T>
     static constexpr T Reflect(const T aValue) = delete;
 
     template<>
     constexpr uint8_t Reflect<uint8_t>(const uint8_t aValue){
-        return REFLECTED_BYTES[aValue];
+        return MathsInternal::REFLECTED_BYTES[aValue];
     }
 
     template<>
     constexpr uint16_t Reflect<uint16_t>(const uint16_t aValue){
-        return (static_cast<uint16_t>(REFLECTED_BYTES[aValue & 0xFF]) << 8) | REFLECTED_BYTES[aValue >> 8];
+        return
+            (static_cast<uint16_t>(Reflect<uint8_t>(aValue >> 8))) |
+            static_cast<uint16_t>(Reflect<uint8_t>(aValue & 0xFF) << 8);
     }
 
     template<>
     constexpr uint32_t Reflect<uint32_t>(const uint32_t aValue){
-        return (static_cast<uint32_t>(Reflect<uint16_t>(aValue & 0xFFFF)) << 16) | Reflect<uint16_t>(aValue >> 16);
+        return
+            (static_cast<uint32_t>(Reflect<uint8_t>(aValue >> 24))) |
+            (static_cast<uint32_t>(Reflect<uint8_t>(aValue >> 16)) << 8) |
+            (static_cast<uint32_t>(Reflect<uint8_t>(aValue >> 8)) << 16) |
+            static_cast<uint32_t>(Reflect<uint8_t>(aValue & 0xFF) << 24);
     }
 
     template<>
     constexpr uint64_t Reflect<uint64_t>(const uint64_t aValue){
-        return (static_cast<uint64_t>(Reflect<uint32_t>(aValue & 0xFFFFFFFFL)) << 32L) | Reflect<uint32_t>(aValue >> 32L);
+        return
+            (static_cast<uint64_t>(Reflect<uint8_t>(aValue >> 56L))) |
+            (static_cast<uint64_t>(Reflect<uint8_t>(aValue >> 48L)) << 8L) |
+            (static_cast<uint64_t>(Reflect<uint8_t>(aValue >> 40L)) << 16L) |
+            (static_cast<uint64_t>(Reflect<uint8_t>(aValue >> 32L)) << 24L) |
+            (static_cast<uint64_t>(Reflect<uint8_t>(aValue >> 24L)) << 32L) |
+            (static_cast<uint64_t>(Reflect<uint8_t>(aValue >> 16L)) << 40L) |
+            (static_cast<uint64_t>(Reflect<uint8_t>(aValue >> 8L)) << 48L) |
+            static_cast<uint64_t>(Reflect<uint8_t>(aValue & 0xFFL) << 56L);
     }
 
     template<>
