e79e03acae9f0b47497d2cddc708f0119611bd21
SolaireLibrary <solairelibrary@mail.com>
2015-11-10 14:11:14 +0000

    Completed type_trait intergration

--- Include/Solaire/Core/Init.hpp
+++ Include/Solaire/Core/Init.hpp
@@ -73,10 +73,6 @@ namespace Solaire{
 	struct TypeTraitData{
 		const uint16_t Size;
 		const struct {
-			// Solaire
-			uint8_t IsSerialisable : 1;
-		};
-		const struct {
 			// STL Primary
 			uint16_t IsArray : 1;
 			uint16_t IsClass : 1;
@@ -92,16 +88,7 @@ namespace Solaire{
 			uint16_t IsVoid : 1;
 			uint16_t IsFloatingPoint : 1;
 		};
-		const struct {
-			// STL Composite
-			uint8_t IsArithmetic : 1;
-			uint8_t IsCompound : 1;
-			uint8_t IsFundamental : 1;
-			uint8_t IsMemberPointer : 1;
-			uint8_t IsReference : 1;
-			uint8_t IsScalar : 1;
-			uint8_t IsObject : 1;
-		};
+
 		const struct {
 			// STL Properties
 			uint16_t IsAbstract : 1;
@@ -116,33 +103,44 @@ namespace Solaire{
 			uint16_t IsTrivial : 1;
 			uint16_t IsVolatile : 1;
 		};
+
 		const struct {
 			// STL Features
 			uint16_t HasVirtualDestructor : 1;
-			uint16_t IsAssignable : 1;
-			uint16_t IsConstructable : 1;
 			uint16_t IsCopyAssignable : 1;
 			uint16_t IsCopyConstructable : 1;
 			uint16_t IsMoveAssignable : 1;
 			uint16_t IsMoveConstructable : 1;
 			uint16_t IsDestructable : 1;
 			uint16_t IsDefaultDestructable : 1;
-			uint16_t IsTriviallyAssignable : 1;
-			uint16_t IsTriviallyConstructable : 1;
 			uint16_t IsTriviallyCopyAssignable : 1;
 			uint16_t IsTriviallyCopyConstructable : 1;
 			uint16_t IsTriviallyMoveAssignable : 1;
 			uint16_t IsTriviallyMoveConstructable : 1;
 			uint16_t IsTriviallyDestructable : 1;
 			uint16_t IsTriviallyDefaultDestructable : 1;
-			uint16_t IsNoThrowAssignable : 1;
-			uint16_t IsNoThrowConstructable : 1;
 			uint16_t IsNoThrowCopyAssignable : 1;
 			uint16_t IsNoThrowCopyConstructable : 1;
 			uint16_t IsNoThrowMoveAssignable : 1;
 			uint16_t IsNoThrowMoveConstructable : 1;
 			uint16_t IsNoThrowDestructable : 1;
 			uint16_t IsNoThrowDefaultDestructable : 1;
+		};
+
+		const struct {
+			// STL Composite
+			uint8_t IsArithmetic : 1;
+			uint8_t IsCompound : 1;
+			uint8_t IsFundamental : 1;
+			uint8_t IsMemberPointer : 1;
+			uint8_t IsReference : 1;
+			uint8_t IsScalar : 1;
+			uint8_t IsObject : 1;
+		};
+
+		const struct {
+			// Solaire
+			uint8_t IsSerialisable : 1;
 		};
 
 		constexpr TypeTraitData(
@@ -154,9 +152,6 @@ namespace Solaire{
 			const uint32_t aFlagsStlFeatures
 		):
 			Size(aSize),
-
-			// Solaire
-			IsSerialisable(					(aFlagsSolaire >> 0) & 1),
 
 			// STL Primary
 			IsArray(						(aFlagsStlPrimary >> 0) & 1),
@@ -172,15 +167,6 @@ namespace Solaire{
 			IsUnion(						(aFlagsStlPrimary >> 10) & 1),
 			IsVoid(							(aFlagsStlPrimary >> 11) & 1),
 			IsFloatingPoint(				(aFlagsStlPrimary >> 12) & 1),
-
-			// STL Composite
-			IsArithmetic(					(aFlagsStlComposite >> 0) & 1),
-			IsCompound(						(aFlagsStlComposite >> 1) & 1),
-			IsFundamental(					(aFlagsStlComposite >> 2) & 1),
-			IsMemberPointer(				(aFlagsStlComposite >> 3) & 1),
-			IsReference(					(aFlagsStlComposite >> 4) & 1),
-			IsScalar(						(aFlagsStlComposite >> 5) & 1),
-			IsObject(						(aFlagsStlComposite >> 6) & 1),
 			
 			// STL Properties
 			IsAbstract(						(aFlagsStlProperties >> 0) & 1),
@@ -197,30 +183,36 @@ namespace Solaire{
 
 			// STL Features
 			HasVirtualDestructor(			(aFlagsStlFeatures >> 0) & 1),
-			IsAssignable(					(aFlagsStlFeatures >> 1) & 1),
-			IsConstructable(				(aFlagsStlFeatures >> 2) & 1),
-			IsCopyAssignable(				(aFlagsStlFeatures >> 3) & 1),
-			IsCopyConstructable(			(aFlagsStlFeatures >> 4) & 1),
-			IsMoveAssignable(				(aFlagsStlFeatures >> 5) & 1),
-			IsMoveConstructable(			(aFlagsStlFeatures >> 6) & 1),
-			IsDestructable(					(aFlagsStlFeatures >> 7) & 1),
-			IsDefaultDestructable(			(aFlagsStlFeatures >> 8) & 1),
-			IsTriviallyAssignable(			(aFlagsStlFeatures >> 9) & 1),
-			IsTriviallyConstructable(		(aFlagsStlFeatures >> 10) & 1),
-			IsTriviallyCopyAssignable(		(aFlagsStlFeatures >> 11) & 1),
-			IsTriviallyCopyConstructable(	(aFlagsStlFeatures >> 12) & 1),
-			IsTriviallyMoveAssignable(		(aFlagsStlFeatures >> 13) & 1),
-			IsTriviallyMoveConstructable(	(aFlagsStlFeatures >> 14) & 1),
-			IsTriviallyDestructable(		(aFlagsStlFeatures >> 15) & 1),
-			IsTriviallyDefaultDestructable(	(aFlagsStlFeatures >> 16) & 1),
-			IsNoThrowAssignable(			(aFlagsStlFeatures >> 17) & 1),
-			IsNoThrowConstructable(			(aFlagsStlFeatures >> 18) & 1),
-			IsNoThrowCopyAssignable(		(aFlagsStlFeatures >> 19) & 1),
-			IsNoThrowCopyConstructable(		(aFlagsStlFeatures >> 20) & 1),
-			IsNoThrowMoveAssignable(		(aFlagsStlFeatures >> 21) & 1),
-			IsNoThrowMoveConstructable(		(aFlagsStlFeatures >> 22) & 1),
-			IsNoThrowDestructable(			(aFlagsStlFeatures >> 23) & 1),
-			IsNoThrowDefaultDestructable(	(aFlagsStlFeatures >> 24) & 1)
+			IsCopyAssignable(				(aFlagsStlFeatures >> 1) & 1),
+			IsCopyConstructable(			(aFlagsStlFeatures >> 2) & 1),
+			IsMoveAssignable(				(aFlagsStlFeatures >> 3) & 1),
+			IsMoveConstructable(			(aFlagsStlFeatures >> 4) & 1),
+			IsDestructable(					(aFlagsStlFeatures >> 5) & 1),
+			IsDefaultDestructable(			(aFlagsStlFeatures >> 6) & 1),
+			IsTriviallyCopyAssignable(		(aFlagsStlFeatures >> 7) & 1),
+			IsTriviallyCopyConstructable(	(aFlagsStlFeatures >> 8) & 1),
+			IsTriviallyMoveAssignable(		(aFlagsStlFeatures >> 9) & 1),
+			IsTriviallyMoveConstructable(	(aFlagsStlFeatures >> 10) & 1),
+			IsTriviallyDestructable(		(aFlagsStlFeatures >> 11) & 1),
+			IsTriviallyDefaultDestructable(	(aFlagsStlFeatures >> 12) & 1),
+			IsNoThrowCopyAssignable(		(aFlagsStlFeatures >> 13) & 1),
+			IsNoThrowCopyConstructable(		(aFlagsStlFeatures >> 14) & 1),
+			IsNoThrowMoveAssignable(		(aFlagsStlFeatures >> 15) & 1),
+			IsNoThrowMoveConstructable(		(aFlagsStlFeatures >> 16) & 1),
+			IsNoThrowDestructable(			(aFlagsStlFeatures >> 17) & 1),
+			IsNoThrowDefaultDestructable(	(aFlagsStlFeatures >> 18) & 1),
+
+			// STL Composite
+			IsArithmetic(					(aFlagsStlComposite >> 0) & 1),
+			IsCompound(						(aFlagsStlComposite >> 1) & 1),
+			IsFundamental(					(aFlagsStlComposite >> 2) & 1),
+			IsMemberPointer(				(aFlagsStlComposite >> 3) & 1),
+			IsReference(					(aFlagsStlComposite >> 4) & 1),
+			IsScalar(						(aFlagsStlComposite >> 5) & 1),
+			IsObject(						(aFlagsStlComposite >> 6) & 1),
+
+			// Solaire
+			IsSerialisable(					(aFlagsSolaire >> 0) & 1)
 		{}
 
 		template<class T>
@@ -229,73 +221,65 @@ namespace Solaire{
 				sizeof(T),
 
 				// Solaire
-				(static_cast<uint32_t>(aSerialiseable)								<< 0),
+				(static_cast<uint32_t>(aSerialiseable)										<< 0),
 
 				// STL Primary
-				(static_cast<uint32_t>(std::is_array<T>::value)						<< 0) |
-				(static_cast<uint32_t>(std::is_class<T>::value)						<< 1) |
-				(static_cast<uint32_t>(std::is_enum<T>::value)						<< 2) |
-				(static_cast<uint32_t>(std::is_function<T>::value)					<< 3) |
-				(static_cast<uint32_t>(std::is_integral<T>::value)					<< 4) |
-				(static_cast<uint32_t>(std::is_lvalue_reference<T>::value)			<< 5) |
-				(static_cast<uint32_t>(std::is_rvalue_reference<T>::value)			<< 6) |
-				(static_cast<uint32_t>(std::is_member_function_pointer<T>::value)	<< 7) |
-				(static_cast<uint32_t>(std::is_member_object_pointer<T>::value)		<< 8) |
-				(static_cast<uint32_t>(std::is_pointer<T>::value)					<< 9) |
-				(static_cast<uint32_t>(std::is_union<T>::value)						<< 10) |
-				(static_cast<uint32_t>(std::is_void<T>::value)						<< 11) |
-				(static_cast<uint32_t>(std::is_floating_point<T>::value)			<< 12),
+				(static_cast<uint32_t>(std::is_array<T>::value)								<< 0) |
+				(static_cast<uint32_t>(std::is_class<T>::value)								<< 1) |
+				(static_cast<uint32_t>(std::is_enum<T>::value)								<< 2) |
+				(static_cast<uint32_t>(std::is_function<T>::value)							<< 3) |
+				(static_cast<uint32_t>(std::is_integral<T>::value)							<< 4) |
+				(static_cast<uint32_t>(std::is_lvalue_reference<T>::value)					<< 5) |
+				(static_cast<uint32_t>(std::is_rvalue_reference<T>::value)					<< 6) |
+				(static_cast<uint32_t>(std::is_member_function_pointer<T>::value)			<< 7) |
+				(static_cast<uint32_t>(std::is_member_object_pointer<T>::value)				<< 8) |
+				(static_cast<uint32_t>(std::is_pointer<T>::value)							<< 9) |
+				(static_cast<uint32_t>(std::is_union<T>::value)								<< 10) |
+				(static_cast<uint32_t>(std::is_void<T>::value)								<< 11) |
+				(static_cast<uint32_t>(std::is_floating_point<T>::value)					<< 12),
 
 				// STL Composite
-				(static_cast<uint32_t>(std::is_arithmetic<T>::value)				<< 0) |
-				(static_cast<uint32_t>(std::is_compound<T>::value)					<< 1) |
-				(static_cast<uint32_t>(std::is_fundamental<T>::value)				<< 2) |
-				(static_cast<uint32_t>(std::is_member_pointer<T>::value)			<< 3) |
-				(static_cast<uint32_t>(std::is_reference<T>::value)					<< 4) |
-				(static_cast<uint32_t>(std::is_scalar<T>::value)					<< 5) |
-				(static_cast<uint32_t>(std::is_object<T>::value)					<< 6),
+				(static_cast<uint32_t>(std::is_arithmetic<T>::value)						<< 0) |
+				(static_cast<uint32_t>(std::is_compound<T>::value)							<< 1) |
+				(static_cast<uint32_t>(std::is_fundamental<T>::value)						<< 2) |
+				(static_cast<uint32_t>(std::is_member_pointer<T>::value)					<< 3) |
+				(static_cast<uint32_t>(std::is_reference<T>::value)							<< 4) |
+				(static_cast<uint32_t>(std::is_scalar<T>::value)							<< 5) |
+				(static_cast<uint32_t>(std::is_object<T>::value)							<< 6),
 
 				// STL Properties
-				/*uint64_t IsAbstract : 1;
-				uint64_t IsConst : 1;
-				uint64_t IsEmpty : 1;
-				uint64_t IsLiteralType : 1;
-				uint64_t IsPod : 1;
-				uint64_t IsPolymorphic : 1;
-				uint64_t IsSigned : 1;
-				uint64_t IsUnsigned : 1;
-				uint64_t IsStandardLayout : 1;
-				uint64_t IsTrivial : 1;
-				uint64_t IsVolatile : 1;*/
-				0,
+				(static_cast<uint32_t>(std::is_abstract<T>::value)							<< 0) |
+				(static_cast<uint32_t>(std::is_const<T>::value)								<< 1) |
+				(static_cast<uint32_t>(std::is_empty<T>::value)								<< 2) |
+				(static_cast<uint32_t>(std::is_literal_type<T>::value)						<< 3) |
+				(static_cast<uint32_t>(std::is_pod<T>::value)								<< 4) |
+				(static_cast<uint32_t>(std::is_polymorphic<T>::value)						<< 5) |
+				(static_cast<uint32_t>(std::is_signed<T>::value)							<< 6) |
+				(static_cast<uint32_t>(std::is_unsigned<T>::value)							<< 7) |
+				(static_cast<uint32_t>(std::is_standard_layout<T>::value)					<< 8) |
+				(static_cast<uint32_t>(std::is_trivial<T>::value)							<< 9) |
+				(static_cast<uint32_t>(std::is_volatile<T>::value)							<< 10),
 				
 				// STL Features
-				/*uint64_t HasVirtualDestructor : 1;
-			uint64_t IsAssignable : 1;
-			uint64_t IsConstructable : 1;
-			uint64_t IsCopyAssignable : 1;
-			uint64_t IsCopyConstructable : 1;
-			uint64_t IsMoveAssignable : 1;
-			uint64_t IsMoveConstructable : 1;
-			uint64_t IsDestructable : 1;
-			uint64_t IsDefaultDestructable : 1;
-			uint64_t IsTriviallyAssignable : 1;
-			uint64_t IsTriviallyConstructable : 1;
-			uint64_t IsTriviallyCopyAssignable : 1;
-			uint64_t IsTriviallyCopyConstructable : 1;
-			uint64_t IsTriviallyMoveAssignable : 1;
-			uint64_t IsTriviallyMoveConstructable : 1;
-			uint64_t IsTriviallyDestructable : 1;
-			uint64_t IsTriviallyDefaultDestructable : 1;
-			uint64_t IsNoThrowAssignable : 1;
-			uint64_t IsNoThrowConstructable : 1;
-			uint64_t IsNoThrowCopyAssignable : 1;
-			uint64_t IsNoThrowCopyConstructable : 1;
-			uint64_t IsNoThrowMoveAssignable : 1;
-			uint64_t IsNoThrowMoveConstructable : 1;
-			uint64_t IsNoThrowDestructable : 1;
-			uint64_t IsNoThrowDefaultDestructable : 1; */
-				0
+				(static_cast<uint32_t>(std::has_virtual_destructor<T>::value)				<< 0) |
+				(static_cast<uint32_t>(std::is_copy_assignable<T>::value)					<< 1) |
+				(static_cast<uint32_t>(std::is_copy_constructible<T>::value)				<< 2) |
+				(static_cast<uint32_t>(std::is_move_assignable<T>::value)					<< 3) |
+				(static_cast<uint32_t>(std::is_move_constructible<T>::value)				<< 4) |
+				(static_cast<uint32_t>(std::is_destructible<T>::value)						<< 5) |
+				(static_cast<uint32_t>(std::is_default_constructible<T>::value)				<< 6) |
+				(static_cast<uint32_t>(std::is_trivially_copy_assignable<T>::value)			<< 7) |
+				(static_cast<uint32_t>(std::is_trivially_copy_constructible<T>::value)		<< 8) |
+				(static_cast<uint32_t>(std::is_trivially_move_assignable<T>::value)			<< 9) |
+				(static_cast<uint32_t>(std::is_trivially_move_constructible<T>::value)		<< 10) |
+				(static_cast<uint32_t>(std::is_trivially_destructible<T>::value)			<< 11) |
+				(static_cast<uint32_t>(std::is_trivially_default_constructible<T>::value)	<< 12) |
+				(static_cast<uint32_t>(std::is_nothrow_copy_assignable<T>::value)			<< 13) |
+				(static_cast<uint32_t>(std::is_nothrow_copy_constructible<T>::value)		<< 14) |
+				(static_cast<uint32_t>(std::is_nothrow_move_assignable<T>::value)			<< 15) |
+				(static_cast<uint32_t>(std::is_nothrow_move_constructible<T>::value)		<< 16) |
+				(static_cast<uint32_t>(std::is_nothrow_destructible<T>::value)				<< 17) |
+				(static_cast<uint32_t>(std::is_nothrow_default_constructible<T>::value)		<< 18)
 			);
 		}
 	};
