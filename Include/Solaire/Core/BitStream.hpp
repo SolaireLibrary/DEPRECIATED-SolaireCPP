a29fd1fc63cf7f208531669aeecb93a26c23f92e
SolaireLibrary <solairelibrary@mail.com>
2015-11-13 02:19:35 +0000

    BitStream

---
+++ Include/Solaire/Core/BitStream.hpp
@@ -0,0 +1,69 @@
+#ifndef SOLAIRE_BIT_STREAM_HPP
+#define SOLAIRE_BIT_STREAM_HPP
+
+//Copyright 2015 Adam Smith
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+// Contact :
+// Email             : solairelibrary@mail.com
+// GitHub repository : https://github.com/SolaireLibrary/SolaireCPP
+
+/*!
+	\file BitStream.hpp
+	\brief
+	\author
+	Created			: Adam Smith
+	Last modified	: Adam Smith
+	\version 1.0
+	\date
+	Created			: 13th September 2015
+	Last Modified	: 13th September 2015
+*/
+
+#include <cstdint>
+
+namespace Solaire{
+
+	class BitStream {
+	private:
+		uint8_t* mByte;
+		uint8_t mMask;
+	public:
+		BitStream(void*);
+		BitStream(void*, const uint8_t);
+		~BitStream();
+
+		void SetBit();
+		void ClearBit();
+		bool CheckBit() const;
+
+		void IncrementBit();
+		void DecrementBit();
+
+		void IncrementBit(const uint8_t);
+		void DecrementBit(const uint8_t);
+
+		uint8_t Offset() const;
+
+		void ReadBits(void*, uint32_t);
+		void WriteBits(const void*, uint32_t);
+
+		void ReadBits(void*, const uint8_t, uint32_t);
+		void WriteBits(const void*, const uint8_t, uint32_t);
+	};
+    
+}
+
+
+#endif
---
+++ Src/Solaire/Core/BitStream.cpp
@@ -0,0 +1,145 @@
+//Copyright 2015 Adam Smith
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+// Contact :
+// Email             : solairelibrary@mail.com
+// GitHub repository : https://github.com/SolaireLibrary/SolaireCPP
+
+#include "Solaire\Core\BitStream.hpp"
+
+namespace Solaire{
+
+	// BitStream
+	
+	BitStream::BitStream(void* aByte) :
+		mByte(static_cast<uint8_t*>(aByte)),
+		mMask(1)
+	{}
+
+	BitStream::BitStream(void* aByte, const uint8_t aOffset) :
+		mByte(static_cast<uint8_t*>(aByte)),
+		mMask(1 << aOffset)
+	{}
+
+	BitStream::~BitStream() {
+
+	}
+
+	void BitStream::SetBit() {
+		*mByte |= mMask;
+	}
+
+	void BitStream::ClearBit() {
+		*mByte &= ~ mMask;
+	}
+
+	bool BitStream::CheckBit() const {
+		return (*mByte & mMask) != 0;
+	}
+
+	void BitStream::IncrementBit() {
+		if(mMask == 128) {
+			mMask = 1;
+			++mByte;
+		}else{
+			mMask <<= 1;
+		}
+	}
+
+	void BitStream::DecrementBit() {
+		if(mMask == 1) {
+			mMask = 128;
+			--mByte;
+		}else {
+			mMask >>= 1;
+		}
+	}
+
+	void BitStream::IncrementBit(const uint8_t aOffset) {
+		for(uint8_t i = 0; i < aOffset; ++i) {
+			IncrementBit();
+		}
+	}
+
+	void BitStream::DecrementBit(const uint8_t aOffset) {
+		for(uint8_t i = 0; i < aOffset; ++i) {
+			DecrementBit();
+		}
+	}
+
+	uint8_t BitStream::Offset() const {
+		switch(mMask)
+		{
+		case 1:
+			return 0;
+		case 2:
+			return 1;
+		case 4:
+			return 2;
+		case 8:
+			return 3;
+		case 16:
+			return 4;
+		case 32:
+			return 5;
+		case 64:
+			return 6;
+		case 128:
+			return 7;
+		default:
+			return 0;
+		}
+	}
+
+	void BitStream::ReadBits(void* aByte, uint32_t aCount) {
+		ReadBits(aByte, 0, aCount);
+	}
+
+	void BitStream::WriteBits(const void* aByte, uint32_t aCount) {
+		WriteBits(aByte, 0, aCount);
+	}
+
+	void BitStream::ReadBits(void* aByte, const uint8_t aOffset, uint32_t aCount) {
+		BitStream other(aByte, aOffset);
+
+		while(aCount != 0) {
+			if(CheckBit()){
+				other.SetBit();
+			}else {
+				other.ClearBit();
+			}
+
+			IncrementBit();
+			other.IncrementBit();
+			--aCount;
+		}
+	}
+
+	void BitStream::WriteBits(const void* aByte, const uint8_t aOffset, uint32_t aCount) {
+		BitStream other(const_cast<void*>(aByte), aOffset);
+
+		while(aCount != 0) {
+			if(other.CheckBit()) {
+				SetBit();
+			}else {
+				ClearBit();
+			}
+
+			IncrementBit();
+			other.IncrementBit();
+			--aCount;
+		}
+	}
+    
+}
